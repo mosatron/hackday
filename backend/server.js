b9e420b3e0fa86ff4d84e6f929f162532b82a22c
Mosa Marzroui-Sebdani <mosa.sebdani@appliedtechnology.se>
2019-03-14 20:21:45 +0100

    addingnewfeatutes

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,15 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'db')));
 
-app.get('/:id', (req, res) => {
+app.get('/thumbs/:id', (req, res) => {
   const id = req.params.id;
   res.sendFile(__dirname + `/db/thumbnails/${id}_tn.jpg`);
 })
  
+// app.get('/:id', (req, res) => {
+//   const id = req.params.id;
+//   res.sendFile(__dirname + `/db/thumbnails/${id}_tn.jpg`);
+// })
 
 app.listen(PORT, ()=> console.log(`server is listening to port ${PORT}`));
 
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,21 +1,21 @@
 
+const id = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 window.onload = () => {
- 
   const button = document.getElementById('imagebutton');
-        const imgDiv = document.getElementById("imgDiv");
+        const thumbDiv = document.getElementById("thumbDiv");
         
         button.addEventListener('click', () => {
         button.textContent = 'Hide Images';
         
-        if(imgDiv.style.display === 'none'){
-          imgDiv.style.display = 'block';
+        if(thumbDiv.style.display === 'none'){
+          thumbDiv.style.display = 'block';
           button.textContent = 'Hide Images';
           return;
         }  
 
-        if(imgDiv.childElementCount !== 0){
-           imgDiv.style.display = 'none';
+        if(thumbDiv.childElementCount !== 0){
+           thumbDiv.style.display = 'none';
            button.textContent = 'Show Images';
            return;
         };
@@ -24,18 +24,36 @@ window.onload = () => {
 }
 
 const getImages = async () => {
-  const id = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  let imgDiv = document.getElementById('imgDiv');
+  let thumbDiv = document.getElementById('thumbDiv');
   
   for(let i = 0; i < id.length; i++){
     let img = document.createElement('img');
-        imgDiv.className = "pics"; 
-    const res = await fetch(`http://localhost:3000/${id[i]}`);
+        thumbDiv.className = "pics"; 
+    const res = await fetch(`http://localhost:3000/thumbs/${id[i]}`);
     const url = res.url;
     img.setAttribute('src', url);
     img.setAttribute('id', id[i] );
     img.setAttribute('width', 200);
+    thumbDiv.appendChild(img);
+  }
+getRealImages();  
+return;
+}
+
+const getRealImages = async () => {
+  let imgDiv = document.getElementById('imgDiv');
+      imgDiv.style.display = 'none';
+  
+  for(let i = 0; i < id.length; i++){
+    let img = document.createElement('img');
+        imgDiv.className = "largepics"; 
+    const res = await fetch(`http://localhost:3000/${id[i]}`);
+    const url = res.url;
+    img.setAttribute('src', url);
+    img.className = 'modal-content';
+    img.setAttribute('id', 0 + id[i] );
     imgDiv.appendChild(img);
   }
 return;
 }
+
