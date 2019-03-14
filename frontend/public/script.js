2348fdc6eedc4f3cc606174e43002310af6c1dbf
Mosa Marzroui-Sebdani <mosa.sebdani@appliedtechnology.se>
2019-03-14 22:00:06 +0100

    flexAdded

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -2,24 +2,23 @@
 const id = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 window.onload = () => {
+  getRealImages()
+  const thumbDiv = document.getElementById("thumbDiv");
   const button = document.getElementById('imagebutton');
-        const thumbDiv = document.getElementById("thumbDiv");
-        
         button.addEventListener('click', () => {
         button.textContent = 'Hide Images';
         
-        if(thumbDiv.style.display === 'none'){
-          thumbDiv.style.display = 'block';
-          button.textContent = 'Hide Images';
-          return;
-        }  
-
-        if(thumbDiv.childElementCount !== 0){
-           thumbDiv.style.display = 'none';
-           button.textContent = 'Show Images';
-           return;
-        };
-       getImages()
+    if(thumbDiv.style.display === 'none'){
+      thumbDiv.style.display = 'block';
+      button.textContent = 'Hide Images';
+      return;
+  }  
+    if(thumbDiv.childElementCount !== 0){
+      thumbDiv.style.display = 'none';
+      button.textContent = 'Show Images';
+      return;
+    };
+    getImages()
   });
 }
 
@@ -28,25 +27,24 @@ const getImages = async () => {
   
   for(let i = 0; i < id.length; i++){
     let img = document.createElement('img');
-        thumbDiv.className = "pics"; 
+    thumbDiv.className = "pics"; 
     const res = await fetch(`http://localhost:3000/thumbs/${id[i]}`);
     const url = res.url;
     img.setAttribute('src', url);
     img.setAttribute('id', id[i] );
-    img.setAttribute('width', 200);
+    //img.addEventListener('click', enLargeImage( '0' + id[img]));
+    //img.setAttribute('width', 200);
     thumbDiv.appendChild(img);
   }
-getRealImages();  
-return;
+  return;
 }
 
 const getRealImages = async () => {
   let imgDiv = document.getElementById('imgDiv');
-      imgDiv.style.display = 'none';
+     
   
   for(let i = 0; i < id.length; i++){
     let img = document.createElement('img');
-        imgDiv.className = "largepics"; 
     const res = await fetch(`http://localhost:3000/${id[i]}`);
     const url = res.url;
     img.setAttribute('src', url);
@@ -57,3 +55,8 @@ const getRealImages = async () => {
 return;
 }
 
+const enLargeImage = (id) => {
+  const img = document.getElementById(id);
+  img.style.display = 'block';
+
+}
