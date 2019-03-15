d66c64ca3d682f8b1749a1cc753a5bf4ed8788e5
Mosa Marzroui-Sebdani <mosa.sebdani@appliedtechnology.se>
2019-03-15 10:10:28 +0100

    addedCors

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const cors = require ('cors');
 
+
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
